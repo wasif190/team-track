4c5b5c8ac62fe36ad6ec775a3a21e413d2d46dc0
wasif190 <wasif.devs@gmail.com>
2026-07-25 13:03:41 +0530

    password hashing bcrypt pre hook

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,61 +1,68 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
-  {
-    avatar: {
-      type: {
-        url: String,
-        localPath: String,
-      },
-      default: {
-        url: `https://placehold.co/200x200`,
-        localPath: "",
-      },
+    {
+        avatar: {
+            type: {
+                url: String,
+                localPath: String,
+            },
+            default: {
+                url: `https://placehold.co/200x200`,
+                localPath: "",
+            },
+        },
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+            lowercase: true,
+            index: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullName: {
+            type: String,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+        },
+        isEmailVerified: {
+            type: Boolean,
+            default: false,
+        },
+        refreshToken: {
+            type: String,
+        },
+        forgotPasswordToken: {
+            type: String,
+        },
+        forgotPasswordExpiry: {
+            type: Date,
+        },
+        emailVerificationToken: {
+            type: String,
+        },
+        emailVerificationExpiry: {
+            type: Date,
+        },
     },
-    username: {
-      type: String,
-      required: true,
-      trim: true,
-      unique: true,
-      lowercase: true,
-      index: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
-    fullName: {
-      type: String,
-      trim: true,
-    },
-    password: {
-      type: String,
-      required: [true, "Password is required"],
-    },
-    isEmailVerified: {
-      type: Boolean,
-      default: false,
-    },
-    refreshToken: {
-      type: String,
-    },
-    forgotPasswordToken: {
-      type: String,
-    },
-    forgotPasswordExpiry: {
-      type: Date,
-    },
-    emailVerificationToken: {
-      type: String,
-    },
-    emailVerificationExpiry: {
-      type: Date,
-    },
-  },
-  { timestamps: true },
+    { timestamps: true },
 );
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
 export const User = mongoose.model("User", userSchema);
